5354dee2e4cf439ce5548da8af670f9fed218b56
capsocong <tien262301@gmail.com>
2025-04-02 13:14:04 +0700

    sefactor-structure

---
+++ src/pages/Boards/BoardBar/index.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material'
+import theme from '../../../theme'
+
+function BoardBar() {
+  return (
+    <Box sx={{
+      backgroundColor: 'primary.dark',
+      width: '100%',
+      height: () => theme.Wms.boardBarHeight,
+      display: 'flex',
+      alignItems: 'center'
+    }}>boardBar</Box>
+  )
+}
+
+export default BoardBar
